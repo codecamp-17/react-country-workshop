62e4bcd62fb2b117c8885c1d92a1d2f2e182384b
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-06 11:24:41 +0700

    refactor:seperate component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+
+import './App.scss';
+import BucketList from './components/bucket-list/BucketList';
+import CountryDetail from './components/country-detail/CountryDetail';
+import CountryList from './components/country-list/CountryList';
+
+function App() {
+  return (
+    <div className='app'>
+      {/* LeftSide */}
+      <CountryList />
+
+      {/* RightSide */}
+      <CountryDetail />
+      <BucketList />
+    </div>
+  );
+}
+
+export default App;
