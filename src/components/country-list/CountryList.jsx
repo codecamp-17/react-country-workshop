515c50d6f155adab2d9e95f16e59322b70b31034
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-06 12:00:33 +0700

    fetch-allCountries

--- src/components/country-list/CountryList.jsx
+++ src/components/country-list/CountryList.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function CountryList() {
+  const [allCountries, setAllCountries] = useState([]);
+
+  const fetchAllCountries = async () => {
+    try {
+      const response = await axios.get('https://restcountries.com/v3.1/all');
+      const data = response.data.map((country) => {
+        // Obj Response => Obj {name:Name,flags:Flags}
+        let obj = {};
+        obj.name = country.name;
+        obj.flags = country.flags;
+        // #1
+        return obj;
+      });
+      // #2
+      setAllCountries(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // #3
+    fetchAllCountries();
+  }, []);
   return (
     <main className='main'>
       <header className='header'>
@@ -11,14 +36,17 @@ function CountryList() {
         <button className='search__btn'>search</button>
       </div>
       <div className='country'>
-        <div className='country__item'>
-          <div className='country__image--container'>
-            <img className='country__image ' alt='flag' />
-          </div>
-          <div className='country__detail'>
-            <h6>country name</h6>
-          </div>
-        </div>
+        {allCountries.length > 0 &&
+          allCountries.map((country) => (
+            <div className='country__item'>
+              <div className='country__image--container'>
+                <img className='country__image' src={country.flags.png} alt={country.flags.alt} />
+              </div>
+              <div className='country__detail'>
+                <h6>{country.name.common}</h6>
+              </div>
+            </div>
+          ))}
       </div>
     </main>
   );
